97b1da8665b012763729bf3a78cee86b0563b21d
TheFreelancer <usashindesadanand@gmail.com>
2024-09-23 09:14:10 -0700

    lenis  is included

--- index.js
+++ index.js
@@ -10,3 +10,34 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf)
+  gsap.registerPlugin(ScrollTrigger)
+ let elem = document.querySelectorAll(".elem")
+ elem.forEach(elem =>{
+  let image = elem.querySelector("img")
+ let tl = gsap.timeline()
+let xTransfrom = gsap.utils.random(-100,100);
+
+tl.set(image,{transfromOrigin:`${xTransfrom <0 ? 0 : "100%"}`,
+},"start")
+.to(image,{
+scale:0,
+ease:"none",
+scrollTrigger:{
+  trigger:image,
+  start:"top top",
+  end:"bottom top",
+  scrub:true,
+}
+},"start")
+.to(elem,{
+  xPercent:xTransfrom,
+  ease:"Power4.easeInOut",
+  scrollTrigger:{
+    trigger:image,
+    start:"top bottom",
+    end:"bottom top",
+    scrub:true,
+  }
+
+})
+})
